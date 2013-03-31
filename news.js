8adc4a8e67424e260c5d7c8e620d0b29e8af2621
Restaurant Laine <mrnsr_sy321@yahoo.com>
2013-03-31 22:29:25 +0900

    updated js

--- news.js
+++ news.js
@@ -1,30 +1,71 @@
 "use strict";
 
+//Twitterオブジェクトのコンストラクタ・プロトタイプ定義
+function Twitter() {}
+Twitter.prototype = {
+        consumerKey:    "CT0K1CufZIY0w3Ir2PbYzQ",
+        consumerSecret: "zgbjko7BrZE9Y23jTMMvksrk7L72311kIBVUHTrj6Gg",
+        accessToken:    "304458004-XcX6oCqw2oUr8FnoZElLDng5veBCB8jY6EJ2CiRn",
+        tokenSecret:    "W9JPu0kbf2VDqlPjBUEEgmBGLWGc7HSnEiMwCd1kGw"
+};
+Twitter.prototype.get = function(api, content) {
+    var accessor = {
+        consumerSecret: this.consumerSecret,
+        tokenSecret: this.tokenSecret
+    };
+
+    var message = {
+        method: "GET",
+        action: api,
+        parameters: {
+            oauth_version: "1.0",
+            oauth_signature_method: "HMAC-SHA1",
+            oauth_consumer_key: this.consumerKey,
+            oauth_token: this.accessToken
+        }
+    };
+    for (var key in content) {
+        message.parameters[key] = content[key];
+    }
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+    var target = OAuth.addToURL(message.action, message.parameters);
+ 
+    var options = {
+        type: message.method,
+        url: target,
+        dataType: "jsonp",
+        jsonp: false,
+        cache: true
+    };
+    $.ajax(options);
+}
+
+function update(data){
+      var root = jQuery('#news');
+      if (data.length) {
+        root.empty();
+        jQuery.each(data.slice(0,5), function(idx, item) {
+          var cDate = new Date(item.created_at);
+                root.append('<h3>' + jQuery.datepicker.formatDate('yy.mm.dd', cDate) + '</h3>');
+                root.append('<p>' + item.text + '</p>');
+              });
+      }
+      else {
+        root.text('最新情報はありません。');
+      }
+}
+
 var feeds = feeds || {};
 
 jQuery.extend(feeds,
   (function() {
-    var f = function getnewsfor(feed) {
-      jQuery.getJSON(
-        'http://search.twitter.com/search.json?q=from:'+feed+'+&callback=?&format=json',
-        function (data) {
-          var root = jQuery('#news');
-          if (data.results && data.results.length) {
-            root.empty();
-            jQuery.each(data.results.slice(0,5), function(idx, item) {
-              var cDate = new Date(item.created_at);
-    		        root.append('<h3>' + jQuery.datepicker.formatDate('yy.mm.dd', cDate) + '</h3>');
-    		        root.append('<p>' + item.text + '</p>');
-    		      });
-          }
-          else if (data.results) {
-            root.text('最新情報はありません。');
-          }
-          else {
-            root.text('最新情報の読み込みに失敗しました。');
-          }
-        }
-      );
+    var twitter = new Twitter(),
+        f = function getnewsfor(feed) {
+        //オプションとコールバック関数の指定
+        var content = {count: "5", callback: "update"};
+        //Twitter APIの呼び出し
+        twitter.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+feed+"&exclude_replies=true&trim_user=true&include_rts=false&since_id=168970566731173888", content)
     }, twitter_feed = 'restaurantlaine';
 
     return {
